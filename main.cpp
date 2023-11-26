a4ecee0eff1b2b591311ce222f2b53a139fc8574
arnob016 <arnob.social@gmail.com>
2023-11-26 11:00:41 +0600

    up

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ float cloudY = 260.0f;
 float cloudSpeedY = 0.001f;
 float sunX = 150.0f;
 float sunY = 300.0f;
+float carX = 50.0f;
+float carY = 25.0f;
+
 
 void init(void)
 {
@@ -81,10 +84,10 @@ void updateSun()
         sunX = 350.0f;
     }
 }
-
 void update(int value)
 {
     cloudX += 0.5f;
+    carX += 1.0f;
     updateSun();
     glutPostRedisplay();
     glutTimerFunc(16, update, 0);
@@ -107,14 +110,23 @@ void specialKey(int key, int x, int y)
 }
 
 void mountain(){
+
     glBegin(GL_TRIANGLES);
-    glColor3f(1.0, 0, 0);
+    glColor3f(0.7, 0.7, 0.7);
+    glVertex2f(180.0, 20.0);
+    glVertex2f(280.0, 20.0);
+    glVertex2f(240.0, 200.0);
+    glEnd();
+    glBegin(GL_TRIANGLES);
+    glColor3f(0.5, 0.5, 0.5);
     glVertex2f(100.0, 20.0);
     glVertex2f(300.0, 20.0);
     glVertex2f(200.0, 250.0);
     glEnd();
 
+
 }
+
 
 void road(){
 
@@ -146,6 +158,119 @@ void road(){
     glEnd();
 }
 
+void car()
+{
+    glPushMatrix();
+    glTranslatef(carX, carY, 0.0f);
+    glBegin(GL_POLYGON);
+    glColor3f(0.5, 0.5, 0.0);
+    glVertex2f( 85.0,20.0 );
+    glVertex2f( 95.0,30.0 );
+    glVertex2f( 105.0,30.0 );
+    glVertex2f( 110.0,38.0 );
+    glVertex2f( 145.0,38.0 );
+    glVertex2f( 150.0,30.0 );
+    glVertex2f( 155.0,30.0 );
+    glVertex2f( 165.0,20.0 );
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.2, 0.2, 0.2);
+    glVertex2f( 102.0, 30.0 );
+    glVertex2f( 125.0, 30.0 );
+    glVertex2f( 125.0, 37.0 );
+    glVertex2f( 110.0, 37.0 );
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.2, 0.2, 0.2);
+    glVertex2f( 148.0, 30.0 );
+    glVertex2f( 126.0, 30.0 );
+    glVertex2f( 126.0, 37.0 );
+    glVertex2f( 144.0, 37.0 );
+    glEnd();
+
+
+    CircleFunc(140.0f, 22.0f, 6.0f, 0,0,0);
+    CircleFunc(110.0f, 22.0f, 6.0f, 0,0,0);
+    CircleFunc(140.0f, 22.0f, 3.0f, 0.7, 0.7, 0.7);
+    CircleFunc(110.0f, 22.0f, 3.0f, 0.7, 0.7, 0.7);
+
+
+
+    glPopMatrix();
+}
+
+void streetLight(float x, float y)
+{
+    glColor3f(0.0f, 0.0f, 0.1f);
+    glLineWidth(5.0);
+    glBegin(GL_LINES);
+    glVertex2f(x, y + 35.0);
+    glVertex2f(x, y + 85.0);
+    glEnd();
+
+    glColor3f(1.0, 1.0, 0.0);
+    CircleFunc(x, y + 85.0, 10.0, 1.0, 1.0, 0.0);
+}
+
+void roadLight(){
+streetLight(-40.0, 35.0);
+streetLight(0.0, 35.0);
+streetLight(40.0, 35.0);
+streetLight(80.0, 35.0);
+streetLight(120.0, 35.0);
+streetLight(160.0, 35.0);
+streetLight(200.0, 35.0);
+streetLight(240.0, 35.0);
+streetLight(280.0, 35.0);
+streetLight(320.0, 35.0);
+streetLight(350.0, 35.0);
+
+}
+
+void roadstrip(){
+    glBegin(GL_QUADS);
+    glColor3f(0.8, 0.8, 0.8);
+    glVertex2f( 0, 0.0 );
+    glVertex2f( 0.0, 15.0 );
+    glVertex2f( 60.0, 15.0 );
+    glVertex2f( 60.0, 0.0 );
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.8, 0.8, 0.8);
+    glVertex2f( 80, 0.0 );
+    glVertex2f( 80.0, 15.0 );
+    glVertex2f( 140.0, 15.0 );
+    glVertex2f( 140.0, 0.0 );
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.8, 0.8, 0.8);
+    glVertex2f( 160, 0.0 );
+    glVertex2f( 160.0, 15.0 );
+    glVertex2f( 220.0, 15.0 );
+    glVertex2f( 220.0, 0.0 );
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.8, 0.8, 0.8);
+    glVertex2f( 240, 0.0 );
+    glVertex2f( 240.0, 15.0 );
+    glVertex2f( 300.0, 15.0 );
+    glVertex2f( 300.0, 0.0 );
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.8, 0.8, 0.8);
+    glVertex2f( 320, 0.0 );
+    glVertex2f( 320.0, 15.0 );
+    glVertex2f( 360.0, 15.0 );
+    glVertex2f( 360.0, 0.0 );
+    glEnd();
+}
+
 void Draw()
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -172,21 +297,18 @@ void Draw()
     }
     else if (sunX < 220.0f)
     {
-        // Afternoon - light yellow
         r = 1.0f;
         g = 1.0f;
         b = 0.8f;
     }
     else if (sunX < 300.0f)
     {
-        // Sunset - orange/red
         r = 0.7f;
         g = 0.5f;
         b = 0.2f;
     }
     else
     {
-        // Night - dark blue
         r = 0.0f;
         g = 0.0f;
         b = 0.1f;
@@ -207,7 +329,9 @@ void Draw()
 
     Sun();
     cloud();
-
+    car();
+    roadLight();
+    roadstrip();
     glutSwapBuffers();
 }
 
